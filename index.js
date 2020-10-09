29138b191bfe9a4c3b9fd4165d9d0ebb7f629247
Chris Trevino <chrisdadef@gmail.com>
2020-10-09 17:57:47 -0500

    updates

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+//get all the usesrs
+app.get('/users', function (req, res) {
+  res.send(users)
+})
+//get a single user
+app.get('users', function (req, res) {
+  res.send(users)
+})
 
+app.post('/users', (req, res) => {
+  let user = {
+    "_id": 5,
+    "name": "Sampson",
+    "occupation": "lawyer",
+    "avatar": "http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg" 
+  }
+})
 
 /* END - create routes here */
 
