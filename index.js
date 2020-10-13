5040064f1159d07bc2e9df4edb3c3aaf97b65c51
Chris Trevino <chrisdadef@gmail.com>
2020-10-12 23:09:32 -0500

    yup

--- index.js
+++ index.js
@@ -1,54 +1,104 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const users = require('./routers/users')
 const app = express()
 const port = process.env.PORT || 4000 
 
-const { users } = require('./state')
+app.use(bodyParser.json())
 
-/* BEGIN - create routes here */
-//get all the users()
-//this returns all the users in postman /users to display ALL USERS!! 
-app.get('/users', (req, res) => {
-  res.send(users)
-})
-  //get a single user
-app.get('/users/1', (req, res) => {
-  res.send(users[0])
-})
-  //This will POST a user
-app.post('/users', (req, res) => {
-  //we will need to ADD a NEW USER object to add to the USERS array
-  //this is the new user
-  let user = {
-    "_id": 5,
-    "name": "Sampson",
-    "occupation": "lawyer",
-    "avatar": "http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg"
-  }
+app.use(users);
 
-  //now we need to push the new user
-  users.push(user);
+// const { users } = require('./state')
+
+// /* BEGIN - create routes here */
+// //get all the users()
+// //this returns all the users in postman /users to display ALL USERS!! 
+// app.get('/users', (req, res) => {
+//   res.send(users)
+// })
+
+
+
+
+
+//   //get a single user
+// app.get('/users/:userId', (req, res) => {
+//   //I'm not too sure what this logs or what it's called
+//   // console.log(req.params);
+//   //obviously need a user id varible 
+//   //need to read up more on this subject of parse int
+//   //but baiscally without parse Int it will return a string not a NUMBER;
+//   let userId = parseInt(req.params.userId);
+//   //using the find method to locate and make sure the ID's MATCH
+//   let user = users.find(user => user._id === userId);
+
+//   res.send(user);
+
+
+
+// })
+
+
+
+
+//   //This will POST a user
+// app.post('/users', (req, res) => {
+//   //gotta console.log req.body to display everything in the body
+//   // console.log(req.body);
+//   //create a user simply by displaying body of that user
+//   let user = req.body;
+//   //this should naturally increment the id on its own to keep users from having the same ID
+//   user._id = users.length + 1;
+
+//   //we will need to ADD a NEW USER object to add to the USERS array
+//   //now we need to push the new user
+//   users.push(user);
+
   
-  //We will need to return the newly created user
-  res.send(user);
-})
-  //This will update a specific user!
-app.put('/users/1', (req, res) => {
-  //need to look up a user
-  let user = users[0];
 
-  //Change values on the user
-  user.name = "Jane";  
-  //return the updated user!
-  res.send(users)[0]
-})
-//removes users from the array
-app.delete('/users', (req,res) => {
-  res.send(users.splice(1,1))
-})
 
-/* END - create routes here */
+//   //We will need to return the newly created user
+//   res.send(users);
+// })
+
+
+
+
+
+//   //This will update a specific user!
+// app.put('/users/:userId', (req, res) => {
+  
+//   let userId = (parseInt(req.params.userId) - 1);
+//   let payload = req.body;
+  
+//   //need to look up a user
+//   // this will find user by its ID thus it looking up the user
+//   let user = users.find(user => user._id === userId);
+
+//   //Change values on the user
+//   Object.keys(payload).map(key => {
+//     user[key] = payload[key];
+//   });
+//   //return the updated user!
+//   res.send(users)
+// })
+
+
+
+
+
+// //removes users from the array
+// app.delete('/users/:userId', (req,res) => {
+//   let userId = parseInt(req.params.userId);
+//   let user = users.find(user => user._id === userId);
+//   user.isActive = false;
+
+
+//   res.send('Deleted!!')
+// })
+
+// /* END - create routes here */
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
---
+++ users.js
@@ -0,0 +1,101 @@
+
+const express = require('express')
+
+const router = express.Router()
+
+const {users} = require('../state')
+
+// Now we simply use our "router" just like our "router" to create routes.
+
+/* BEGIN - create routes here */
+//get all the users()
+//this returns all the users in postman /users to display ALL USERS!! 
+router.get('/users', (req, res) => {
+  res.send(users)
+})
+
+
+
+
+
+  //get a single user
+router.get('/users/:userId', (req, res) => {
+  //I'm not too sure what this logs or what it's called
+  // console.log(req.params);
+  //obviously need a user id varible 
+  //need to read up more on this subject of parse int
+  //but baiscally without parse Int it will return a string not a NUMBER;
+  let userId = parseInt(req.params.userId);
+  //using the find method to locate and make sure the ID's MATCH
+  let user = users.find(user => user._id === userId);
+
+  res.send(user);
+
+
+
+})
+
+
+
+
+  //This will POST a user
+router.post('/users', (req, res) => {
+  //gotta console.log req.body to display everything in the body
+  // console.log(req.body);
+  //create a user simply by displaying body of that user
+  let user = req.body;
+  //this should naturally increment the id on its own to keep users from having the same ID
+  user._id = users.length + 1;
+
+  //we will need to ADD a NEW USER object to add to the USERS array
+  //now we need to push the new user
+  users.push(user);
+
+  
+
+
+  //We will need to return the newly created user
+  res.send(users);
+})
+
+
+
+
+
+  //This will update a specific user!
+router.put('/users/:userId', (req, res) => {
+  
+  let userId = (parseInt(req.params.userId) - 1);
+  let payload = req.body;
+  
+  //need to look up a user
+  // this will find user by its ID thus it looking up the user
+  let user = users.find(user => user._id === userId);
+
+  //Change values on the user
+  Object.keys(payload).map(key => {
+    user[key] = payload[key];
+  });
+  //return the updated user!
+  res.send(user)
+})
+
+
+
+
+
+//removes users from the array
+router.delete('/users/:userId', (req,res) => {
+  let userId = parseInt(req.params.userId);
+  let user = users.find(user => user._id === userId);
+  user.isActive = false;
+
+
+  res.send('Deleted!!')
+})
+
+/* END - create routes here */
+
+// And then we export the router:
+
+module.exports = router
